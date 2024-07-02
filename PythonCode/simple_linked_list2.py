3a7247984189931dc4eaa850ee6b4fc8428531cf
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-07-02 21:38:58 +0800

    Create simple_linked_list2.py

---
+++ PythonCode/simple_linked_list2.py
@@ -0,0 +1,32 @@
+# simple_linked_list2.py
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+        print("None")
+
+if __name__ == "__main__":
+    llist = LinkedList()
+    for i in range(5):
+        llist.append(i)
+    llist.display()
