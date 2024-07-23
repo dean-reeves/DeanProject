20b16ba370e2dc6b5c6e79c36ca194f87e305b3a
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-07-23 22:22:02 +0800

    Create fun_py_calculate_simple_interest.py

---
+++ PythonCode/fun_py_calculate_simple_interest.py
@@ -0,0 +1,5 @@
+# fun_py_calculate_simple_interest.py
+def fun_py_calculate_simple_interest(principal, rate, time):
+    return (principal * rate * time) / 100
+
+print(fun_py_calculate_simple_interest(1000, 5, 2))
