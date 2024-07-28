af6e580a484363623b62a494f09a6c78a9e48185
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-07-28 11:30:02 +0800

    Create func_py_generate_random_integer.py

---
+++ PythonCode/func_py_generate_random_integer.py
@@ -0,0 +1,7 @@
+# func_py_generate_random_integer.py
+import random
+
+def func_py_generate_random_integer(start, end):
+    return random.randint(start, end)
+
+print(func_py_generate_random_integer(1, 100))
