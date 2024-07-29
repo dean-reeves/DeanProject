ead85a870a5a17e6fa69c9520e2a83ab74da9886
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-07-29 17:34:53 +0800

    Create func_py_generate_random_choice.py

---
+++ PythonCode/func_py_generate_random_choice.py
@@ -0,0 +1,7 @@
+# func_py_generate_random_choice.py
+import random
+
+def func_py_generate_random_choice(lst):
+    return random.choice(lst)
+
+print(func_py_generate_random_choice([1, 2, 3, 4, 5]))
