5f27e31d48ce7e1dd7a4147a23001827b914d12b
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-08-06 18:53:53 +0800

    Create func_py_generate_random_float_in_range.py

---
+++ PythonCode/func_py_generate_random_float_in_range.py
@@ -0,0 +1,7 @@
+# func_py_generate_random_float_in_range.py
+import random
+
+def func_py_generate_random_float_in_range(start, end):
+    return random.uniform(start, end)
+
+print(func_py_generate_random_float_in_range(1.0, 5.0))
