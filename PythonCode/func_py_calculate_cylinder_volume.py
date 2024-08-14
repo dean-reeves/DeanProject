51a925e155d09b64a8ee8433a2b3660b16d69587
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-08-14 20:02:59 +0800

    Create func_py_calculate_cylinder_volume.py

---
+++ PythonCode/func_py_calculate_cylinder_volume.py
@@ -0,0 +1,7 @@
+# func_py_calculate_cylinder_volume.py
+import math
+
+def func_py_calculate_cylinder_volume(radius, height):
+    return math.pi * radius**2 * height
+
+print(func_py_calculate_cylinder_volume(3, 7))
