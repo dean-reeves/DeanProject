fe55a13200544c95520b1d749a096e12eb0e3bcb
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-08-18 22:27:27 +0800

    Create func_py_calculate_right_triangular_prism_volume.py

---
+++ PythonCode/func_py_calculate_right_triangular_prism_volume.py
@@ -0,0 +1,5 @@
+# func_py_calculate_right_triangular_prism_volume.py
+def func_py_calculate_right_triangular_prism_volume(base, height, length):
+    return 0.5 * base * height * length
+
+print(func_py_calculate_right_triangular_prism_volume(5, 6, 10))
