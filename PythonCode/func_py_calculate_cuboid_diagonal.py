82798620269425a0fd82da33bcf764c6d56acd00
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-08-19 19:00:37 +0800

    Create func_py_calculate_cuboid_diagonal.py

---
+++ PythonCode/func_py_calculate_cuboid_diagonal.py
@@ -0,0 +1,7 @@
+# func_py_calculate_cuboid_diagonal.py
+import math
+
+def func_py_calculate_cuboid_diagonal(length, width, height):
+    return math.sqrt(length**2 + width**2 + height**2)
+
+print(func_py_calculate_cuboid_diagonal(3, 4, 5))
