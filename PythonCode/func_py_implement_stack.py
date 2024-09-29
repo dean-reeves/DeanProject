b5872d71e59b4af6d58c40e249a4d332628729b6
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2024-09-29 20:01:30 +0800

    Create func_py_implement_stack.py

---
+++ PythonCode/func_py_implement_stack.py
@@ -0,0 +1,20 @@
+# func_py_implement_stack.py
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop() if not self.is_empty() else None
+    
+    def peek(self):
+        return self.items[-1] if not self.is_empty() else None
+    
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def size(self):
+        return len(self.items)
+
