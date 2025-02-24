e2c7b3cee49fd87640312be303122a91279eafdc
dean-reeves <166919847+dean-reeves@users.noreply.github.com>
2025-02-24 14:14:18 +0800

    Create func_py_is_prime.py

---
+++ PythonCode/part2/func_py_is_prime.py
@@ -0,0 +1,10 @@
+# 1. 计算某个数是否为质数
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(29))  # True
